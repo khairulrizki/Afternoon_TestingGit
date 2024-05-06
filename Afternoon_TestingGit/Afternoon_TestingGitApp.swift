8e598bc40c2653e671b3943d922412a99eed037d
khairulrizki <khairulrizkirmd@gmail.com>
2024-05-06 16:33:50 +0700

    Initial Commit

---
+++ Afternoon_TestingGit/Afternoon_TestingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Afternoon_TestingGitApp.swift
+//  Afternoon_TestingGit
+//
+//  Created by Khairul Rizki on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Afternoon_TestingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
