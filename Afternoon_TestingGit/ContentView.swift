957de6d5fea498df696416ccf6a6521fd0ccf6ce
Dwiky SP <dwiky01@gmail.com>
2024-05-06 17:17:13 +0700

    Tambah Fitur

--- Afternoon_TestingGit/ContentView.swift
+++ Afternoon_TestingGit/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello, world!")
+            Text("Wakwaw")
+            Text("cukup")
         }
         .padding()
     }
